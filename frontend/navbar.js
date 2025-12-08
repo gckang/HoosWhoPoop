eaf8f459170307634fc02f8e6f009b494b635344
blairezh <forabzstudent@gmail.com>
2025-12-07 23:46:13 -0500

    removed entries/events page

--- frontend/navbar.js
+++ frontend/navbar.js
@@ -7,7 +7,6 @@
             h("p", null, "Hello pooper, ", h("span", { className: "username" }, user.username)),
             h("ul", { className: "navigation" },
                 h("li", null, h("a", { href: "index.html" }, "Home")),
-                h("li", null, h("a", { href: "events.html" }, "Entries")),
                 h("li", null, h("a", { href: "rooms.html" }, "Rooms"))
             ),
             h("button", { className: "logout-button", onClick: handleLogout }, "Logout")

fd80e1586a9264f915bbe2cd99173f4e9dff559a
blairezh <forabzstudent@gmail.com>
2025-12-08 05:22:27 -0500

    UI cleaning

--- frontend/navbar.js
+++ frontend/navbar.js
@@ -1,15 +1,21 @@
 (function (global) {
-    const { createElement: h, useState } = React;
+    const { createElement: h } = React;
 
     global.NavBar = function NavBar({ user, handleLogout }) {
-        return h("header", { className: "app-header" }, 
-            h("h1", null, "HoosWhoPoop"),
-            h("p", null, "Hello pooper, ", h("span", { className: "username" }, user.username)),
-            h("ul", { className: "navigation" },
-                h("li", null, h("a", { href: "index.html" }, "Home")),
-                h("li", null, h("a", { href: "rooms.html" }, "Rooms"))
+        return h("header", { className: "app-header" },
+            h("div", { className: "app-header-left" },
+                h("h1", null, "HoosWhoPoop"),
+                h("p", null, "Hello pooper, ", h("span", { className: "username" }, user.username))
             ),
-            h("button", { className: "logout-button", onClick: handleLogout }, "Logout")
+            h("div", { className: "app-header-center" },
+                h("ul", { className: "navigation" },
+                    h("li", null, h("a", { href: "index.html" }, "Home")),
+                    h("li", null, h("a", { href: "rooms.html" }, "Rooms"))
+                )
+            ),
+            h("div", { className: "app-header-right" },
+                h("button", { className: "logout-button", onClick: handleLogout }, "Logout")
+            )
         );
     };
 })(window);
